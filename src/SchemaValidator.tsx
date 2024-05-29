9ab582845351f8cc1562d5312502a46d71dc997b
Ananthagari  k <ananthagarik@Admin1s-MacBook-Pro.local>
2024-05-29 11:59:05 +0530

    [kalpana]:created login&signup page

--- src/SchemaValidator.tsx
+++ src/SchemaValidator.tsx
@@ -1,13 +1,26 @@
 
-import React from 'react'
+import {z} from 'zod';
 
-export default function SchemaValidator() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+export const schema = z.object({
+  firstName: z.string().min(1, 'First name is required'),
+  lastName: z.string().min(1, 'Last name is required'),
+  email: z.string().email('Invalid email address').min(1, 'Email is required'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+  confirmPassword: z.string().min(1, 'Confirm Password is required')
+}).superRefine(({ password, confirmPassword }, ctx) => {
+  if (password !== confirmPassword) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'Passwords do not match',
+      path: ['confirmPassword'],
+    });
+  }
+});
+
+
+
+
+
 
 
 
